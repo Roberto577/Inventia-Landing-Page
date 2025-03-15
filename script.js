dbe852cadd457e83e1365b3c6dfb0d49ab8a2067
RobertoDuoc <rob.valenzuela@duocuc.cl>
2025-03-15 19:01:59 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector("button").addEventListener("click", function() {
+    alert("Gracias por tu interés. Te contactaremos pronto.");
+});
+
+//Supongo que es para comenzar el carrusel
+document.addEventListener("DOMContentLoaded", function() {
+    AOS.init();
+});
